0e1d05c6092b46469dfd7fa2600ccd9024946138
Anzar Salam <anzarsalamr@gmail.com>
2022-07-04 23:51:27 +0530

    Temp Jul 4

---
+++ src/components/Landing/Introduction.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Introduction extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Hi,</h4>
+                <h1>I'm Anzar Salam,</h1>
+                <h3>A FullStack Website developer</h3>
+            </div>
+        );
+    }
+}
+
+export default Introduction;
---
+++ src/components/Sorry/UnderConstruction.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class UnderConstruction extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Coming Soon...</h3>
+                <p>This page is under construction.</p>
+            </div>
+        );
+    }
+}
+
+export default UnderConstruction;
